983e11a76624b3f25265c1ae299fa060d1206d94
jovyzabel <jovymonka7@gmail.com>
2024-09-12 23:36:28 +0100

    second commit

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
-const boxMouse = document.querySelector('.box');
-const secondBox = document.querySelector('.secondBox');
-const container = document.querySelector('.container');
+// const boxMouse = document.querySelector('.box');
+// const secondBox = document.querySelector('.secondBox');
+// const container = document.querySelector('.container');
 const secondContainer = document.querySelector('.second-container');
 const hour = document.querySelector('.hour');
 const min = document.querySelector('.min');
 const sec = document.querySelector('.sec');
 
-const spinBox = [
-  { transform: "rotate(0) scale(1)" },
-  { transform: "rotate(360deg) scale(0)" },
-];
+// const spinBox = [
+//   { transform: "rotate(0) scale(1)" },
+//   { transform: "rotate(360deg) scale(0)" },
+// ];
 
 const timing = {
   duration: 1000,
@@ -18,38 +18,39 @@ const timing = {
 
 console.log(boxMouse);
 
-boxMouse.addEventListener('mouseover', (e)=>{
-  boxMouse.style.backgroundColor = '#023e8a';
-  boxMouse.style.boxShadow = '4px 3px 7px orange';
-  boxMouse.style.borderRadius = '100px';
-})
+// boxMouse.addEventListener('mouseover', (e)=>{
+//   boxMouse.style.backgroundColor = '#023e8a';
+//   boxMouse.style.boxShadow = '4px 3px 7px orange';
+//   boxMouse.style.borderRadius = '100px';
+// })
 
-boxMouse.addEventListener('mouseleave', (e)=>{
-  boxMouse.style.backgroundColor = 'orange';
-  boxMouse.style.boxShadow = 'none';
-  boxMouse.style.borderRadius = '8px';
-})
+// boxMouse.addEventListener('mouseleave', (e)=>{
+//   boxMouse.style.backgroundColor = 'orange';
+//   boxMouse.style.boxShadow = 'none';
+//   boxMouse.style.borderRadius = '8px';
+// })
 
-boxMouse.addEventListener('click', (e)=>{
-  boxMouse.style.position = 'absolute';
-  boxMouse.style.top = '185px';
-  secondBox.style.display = 'block';
-  secondBox.animate([
-     { transform: "translateY(0px)" },
-    { transform: "translateX(330px)" },
-  ],
-  {
-    duration: 1000,
-    iterations: Infinity,
-  })
-})
+// boxMouse.addEventListener('click', (e)=>{
+//   boxMouse.style.position = 'absolute';
+//   boxMouse.style.top = '125px';
+//   boxMouse.style.left = '185px';
+//   secondBox.style.display = 'block';
+//   secondBox.animate([
+//      { transform: "translateY(0px)" },
+//     { transform: "translateX(330px)" },
+//   ],
+//   {
+//     duration: 1000,
+//     iterations: Infinity,
+//   })
+// })
 
-boxMouse.addEventListener('dblclick', (e)=>{
-  boxMouse.animate(spinBox, timing)
-})
+// boxMouse.addEventListener('dblclick', (e)=>{
+//   boxMouse.animate(spinBox, timing)
+// })
 
-// container.addEventListener('click', (e)=>{
-//     secondBox.style.display = 'none';
+// container.addEventListener('DOMContentLoaded', (e)=>{
+//     container.style.display = 'none';
 //   })
 let intervalId;
 
---
+++ js/main.js
@@ -0,0 +1,88 @@
+const secondContainer = document.querySelector('.second-container');
+const hour = document.querySelector('.hour');
+const min = document.querySelector('.min');
+const sec = document.querySelector('.sec');
+
+const box = document.querySelector('.box');
+
+const spinBox = [
+    { transform: "rotate(0) scale(1)" },
+    { transform: "rotate(360deg) scale(0)" },
+  ];
+  
+const timing = {
+  duration: 1000,
+  iterations: Infinity,
+}
+
+let intervalId;
+let i = 0;
+let j = 0;
+let k = 0;
+
+let position = [0, 170,-170, 0, -170, 170];
+let positionY = [0, 130, -130, 0, 130, -130];
+let opacities = ['.6','.7','.8'];
+let colors = [
+  'linear-gradient(180deg, rgb(255, 187, 0), rgb(219, 65, 4))',
+  'linear-gradient(90deg, rgb(219, 65, 4),rgb(255, 187, 0))',
+  'linear-gradient(270deg, rgb(255, 187, 0), rgb(219, 65, 4))',
+  'linear-gradient(360deg, rgb(219, 65, 4),rgb(255, 187, 0))',
+  'linear-gradient(60deg,rgb(255, 187, 0), rgb(219, 65, 4))',
+  'linear-gradient(0deg, rgb(219, 65, 4),rgb(255, 187, 0))'
+];
+
+
+const changeDesign = function(){
+  if (!intervalId) {
+    setInterval( manageColor, 1000)
+  }
+}
+
+const manageColor = ()=>{
+  secondContainer.innerHTML = '<div class="circle"></div>';
+  const circle = document.querySelector('.circle')
+  circle.style.transform = `translateX(${position[i]}px) translateY(${positionY[i]}px)`;
+  i= (i+1)%position.length;
+  
+  // circle.style.transform = `translateY(${position[i]}px)`;
+  circle.style.opacity = `${opacities[k]}`; 
+  // i= (i+1)%opacities.length;
+  secondContainer.style.background = `${colors[i]}`; 
+  j= (j+1)%colors.length;
+  // console.log(circle);
+}
+/**
+ * Connexion of the changeDesign function to The DOMContentLoaded event
+ */
+window.addEventListener('DOMContentLoaded', 
+  changeDesign(),
+  box.animate(spinBox, timing)
+) ;
+
+/**
+ * function to get the time
+ * 
+*/
+// const timer = ()=>{
+//   const t= new Date();
+//   hour.textContent = `${t.getHours()}`;
+//   min.textContent = `${t.getMinutes()}`;
+//   sec.textContent = `${t.getSeconds()}`;
+  
+// }
+// setInterval(timer(),1000)
+
+setInterval(()=>{
+  const t= new Date();
+  hour.textContent = `${t.getHours()}`;
+  min.textContent = `${t.getMinutes()}`;
+  sec.textContent = `${t.getSeconds()}`;
+  
+}, 1000);
+
+
+  
+  // window.addEventListener('load', (e)=>{
+  //   box.animate(spinBox, timing)
+  // })
